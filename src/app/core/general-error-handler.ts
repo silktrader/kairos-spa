be7bac23527a7b5db8b2e5473ea5e96e12791516
Silktrader <silktrader@gmail.com>
2020-05-06 08:14:39 +0200

    Display error dialogs within ngZone to avoid update issues

--- src/app/core/general-error-handler.ts
+++ src/app/core/general-error-handler.ts
@@ -1,10 +1,10 @@
-import { Injectable, ErrorHandler, Injector } from '@angular/core';
+import { Injectable, ErrorHandler, Injector, NgZone } from '@angular/core';
 import { NotificationService } from '../services/notification.service';
 
 @Injectable()
 export class GeneralErrorHandler implements ErrorHandler {
   // Because of this we should manually inject the services with Injector.
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector, private ngZone: NgZone) {}
 
   handleError(error: Error) {
     const ns = this.injector.get(NotificationService);
@@ -12,7 +12,8 @@ export class GeneralErrorHandler implements ErrorHandler {
     // tk must handle HTTP errors differently later
 
     // pop up a dialog with a notification
-    ns.warnError(error);
+    // must run within ngZone to avoid lacks of updates
+    this.ngZone.run(() => ns.warnError(error));
 
     // always log the error to the console
     console.error(error);
